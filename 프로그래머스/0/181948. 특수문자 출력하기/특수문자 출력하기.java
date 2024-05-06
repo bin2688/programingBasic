cc2e0200a0253bd10dc3cafc072944a30dcc39a1
Neverend <145637393+bin2688@users.noreply.github.com>
2024-05-06 21:39:33 +0900

    [level 0] Title: 특수문자 출력하기, Time: 139.46 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public static void main(String[] args) {
+        //이스케이프 시퀀스, \와 "는 앞에 백슬래시 추가해야 출력
+        System.out.println("!@#$%^&*(\\'\"<>?:;");       
+    }
+}
