94c09f7025f925bb5163242169e57615dfd4f58d
Neverend <145637393+bin2688@users.noreply.github.com>
2024-06-21 21:08:57 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String[] str = Integer.toString(n).split("");
+        
+		for (String s : str) {
+			answer += Integer.parseInt(s);
+		}
+        
+        return answer;
+    }
+}
